1178ab718d224c8c7c9075e9d81be2f62c6da7aa
hungpv <phamvanhung.dev@gmail.com>
2025-01-02 15:55:28 +0700

    done

--- terminal/crawl.py
+++ terminal/crawl.py
@@ -180,7 +180,7 @@ class Crawl:
             res = handleFile.send_file_to_server(last_file, 'https://admin.rovegl.com/api/receive-excel-data-file')
             
             # Xóa tệp sau khi gửi
-            handleFile.remove_file(last_file)
+            # handleFile.remove_file(last_file)
             
             print(f"File uploaded successfully ==> {self.account['name']}")
 
